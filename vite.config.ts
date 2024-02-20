980c1b27182b45023cf35a577c4ef72bcdcd9509
milicevicrobert <73672164+milicevicrobert@users.noreply.github.com>
2024-02-20 23:31:59 +0100

    treba ruter dodati

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import preprocess from 'svelte-preprocess';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte({
+      // Enable run-time checks when not in production
+      compilerOptions: {
+        dev: !process.env.production,
+      },
+      // Enable preprocessing for Svelte files
+      preprocess: preprocess(),
+    }),
+  ],
+});
